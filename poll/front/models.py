e8828f711ad1cb8fdb4297e4ca1597580144925a
ShubhamKaudewar <kaudewar@gmail.com>
2020-05-26 14:14:03 +0530

    Done

--- poll/front/models.py
+++ poll/front/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Values(models.Model):
     name = models.CharField(max_length=200)
     desc = models.TextField(blank=True)
-    time = models.TextField(blank=True)
+    time = models.CharField(max_length=200)
     view = models.TextField(blank=True)
--- poll/front/views.py
+++ poll/front/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from front.models import Values
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 def Values_view(request):
@@ -14,13 +15,11 @@ def Create(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         desc = request.POST.get('desc')
-        time = request.POST.get('time')
-        view = request.POST.get('views')
 
         Values.objects.create(
             name = name,
             desc = desc,
-            time = time,
-            view = view
+            time = datetime.now(),
+            view = ""
         )
         return HttpResponse('')
